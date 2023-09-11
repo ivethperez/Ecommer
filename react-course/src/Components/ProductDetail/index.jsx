910181542798316be0e3a896b2b1d5688209739b
Iveth Pérez <ivethp62@gmail.com>
2023-09-11 15:03:48 -0500

    Se abre modal para detalle producto

--- react-course/src/Components/ProductDetail/index.jsx
+++ react-course/src/Components/ProductDetail/index.jsx
@@ -9,12 +9,15 @@ const ProductDetail = () => {
     return (
         <div className='flex flex-col fixed bg-white rounded-lg w-[400px] h-[calc(100vh-140px)] overflow-hidden shadow-lg hover:shadow-xl 
 hover:scale-105 duration-500 transform transition'>
-            <figure className='relative mb-2 w-full h-3/5'>
-                <div>
-                    <XMarkIcon className='absolute top-0 right-0 flex justify-center items-center bg-[#2f323a9d] w-6 h-6 rounded-full m-2 p-1 font-bold cursor-pointer'
-                        onClick={onCancel}></XMarkIcon>
-                </div>
 
+<div className='flex justify-between items-center p-6'>
+<h2 className='fount-medium text-xl'>Detalle</h2>
+<button 
+                onClick={onCancel}>
+                    <XMarkIcon className='h-6 w-6'></XMarkIcon>
+                </button>
+                </div>
+            <figure className='relative mb-2 w-full h-3/5'>
                 <img
                     className='w-full h-full object-cover rounded-lg'
                     src={productShow.images[0]}
@@ -33,6 +36,8 @@ hover:scale-105 duration-500 transform transition'>
                     {productShow.description}
                 </p>
             </div>
+            
+
         </div>
     )
 }
