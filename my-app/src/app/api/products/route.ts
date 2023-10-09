fcf2cc5781a07c264d34523f568dd2ff02eea413
Iveth Pérez <ivethp62@gmail.com>
2023-10-09 18:01:33 -0500

    Nueva API

---
+++ my-app/src/app/api/products/route.ts
@@ -0,0 +1,16 @@
+import {type NextRequest, NextResponse } from "next/server";
+export const GET = async (request: NextRequest) =>{
+    try{
+
+        const response = await fetch('https://api-product-5iv7.onrender.com/products')
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+          }
+
+          const products =  await response.json()
+          return NextResponse.json({data:products})
+    }
+    catch (err) {
+        return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+      }  
+}
