e6f0e10cc21699718601099cc6370c439d276d9b
Iveth Pérez <ivethp62@gmail.com>
2023-10-12 14:00:19 -0500

    api

--- my-app/src/app/api/products/route.ts
+++ my-app/src/app/api/products/route.ts
@@ -1,16 +1,19 @@
-import {type NextRequest, NextResponse } from "next/server";
-export const GET = async (request: NextRequest) =>{
-    try{
-
-        const response = await fetch('https://api-product-5iv7.onrender.com/products')
-        if (!response.ok) {
-            throw new Error(`Error! status: ${response.status}`);
+import { NextResponse } from "next/server";
+export const GET = async () =>{
+     try{
+        const response = await fetch(process.env.API_URL!,{
+          headers:{
+            'Content-type': 'application/json'
           }
+        })
+        if (!response.ok) {
+             throw new Error(`Error! status: ${response.status}`);
+           }
 
-          const products =  await response.json()
-          return NextResponse.json({data:products})
-    }
-    catch (err) {
-        return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
-      }  
+           const products =  await response.json()
+           return NextResponse.json({data:products})
+     }
+     catch (err) {
+         return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+       }  
 }
