04df42980e9f5049589d7ee7b993cc3c84a896f3
Iveth <ivethp62@gmail.com>
2024-01-23 19:11:22 -0500

    response

--- react-course/nextroute/src/app/api/products/route.ts
+++ react-course/nextroute/src/app/api/products/route.ts
@@ -6,7 +6,7 @@ export const GET = async () =>
     await connectDB()
     try{
         const result = await ProductsModel.find({})
-        return (NextResponse.json({products:result},{status:200}))
+        return (Response.json({products:result},{status:200}))
     }
     catch(error){
         return NextResponse.json({data:null},{status:500})
